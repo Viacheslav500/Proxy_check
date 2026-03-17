b26ec1f28971e7395feade7f909355203d6a022e
Viacheslav Kurganov <slavanik2506@gmail.com>
2026-03-17 20:36:09 +0300

    Added a proxy in checker.py to test it

--- src/checker.py
+++ src/checker.py
@@ -1,4 +1,9 @@
 import requests
 
-free = requests.get('https://ya.ru/')
+proxy_address = input('Enter proxy address. For example, https://1.1.1.1:0000')
+proxies = {
+        'http': f'{proxy_address}',
+        'https': f'{proxy_address}'
+        }
+free = requests.get('https://ya.ru/', proxies=proxies)
 print(free)
