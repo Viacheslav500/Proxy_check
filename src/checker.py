7ecac5cd715dd61ab378eeb251fd5bc55f8fc885
Viacheslav Kurganov <slavanik2506@gmail.com>
2026-03-28 19:27:37 +0300

    Changed order of exceptions

--- src/checker.py
+++ src/checker.py
@@ -1,24 +1,28 @@
 import requests
 import urllib3
+import http.client
 
-urllib3.disable_warnings
+urllib3.disable_warnings()
 
 proxy_address = input('Enter proxy address. For example, http://ip:port \n')
 proxies = {
-        'http': f'{proxy_address}',
-        'https': f'{proxy_address}'
+        'http': proxy_address,
+        'https': proxy_address
         }
 try:
-    free = requests.get('https://ya.ru/', proxies=proxies, verify=False, timeout=5)
+    free = requests.get('https://ya.ru/', proxies=proxies, verify=False, timeout=10)
     print(free)
+except requests.exceptions.Timeout:
+    print('Timeout occurred')
+except requests.exceptions.ConnectionError:
+    print('Connection Error occurred')
 except requests.exceptions.SSLError:
     print('SSLError occurred')
 except http.client.RemoteDisconnected:
     print('Proxy is unavailable')
 except urllib3.exceptions.ProtocolError:
     print('Protocol Error occurred')
-except requests.exceptions.ConnectionError:
-    print('Conection Error occurred')
+
 
 #free = requests.get('https://ya.ru/', proxies=proxies)
 #print(free)
