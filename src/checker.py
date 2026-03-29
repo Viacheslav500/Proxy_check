9044d5e875d58a1a7b9377cc839a7acb20cc4a87
Viacheslav Kurganov <slavanik2506@gmail.com>
2026-03-29 15:17:41 +0300

    Added a description for two variables, changed variable name

--- src/checker.py
+++ src/checker.py
@@ -4,27 +4,32 @@ import http.client
 
 urllib3.disable_warnings()
 
-proxy_address = input('Enter proxy address. For example, http://ip:port \n')
+proxy_address = input('Enter proxy address. For example, http://ip:port \n').strip()
 proxies = {
         'http': proxy_address,
         'https': proxy_address
         }
 
+#create to variables, 'total' to sum up time for all urls in the 'urls.txt'
+#'success_count' for counting ulrs which answered with status 200
+
 total = 0
+success_count = 0
 
 urls = list()
 
 with open('urls.txt', 'r', encoding='utf-8') as file:
     for line in file:
-        urls.append(line)
+        urls.append(line.strip())
 
 try:
     for url in urls:
-        free = requests.get(url, proxies=proxies, verify=False, timeout=10)
-        elapsed = r.elapsed.total_seconds()
+        valid_proxy = requests.get(url, proxies=proxies, verify=False, timeout=10)
+        elapsed = valid_proxy.elapsed.total_seconds()
         total += elapsed
-
-    print(f'Answer: {free} | Average time: {total // len(urls)}')
+        success_count += 1
+    if success_count > 1:
+        print(f'Proxy: {proxy_address} | Answer: {valid_proxy} | Average time: {total // success_count}')
 except requests.exceptions.Timeout:
     print('Timeout occurred')
 except requests.exceptions.ConnectionError:
@@ -35,7 +40,3 @@ except http.client.RemoteDisconnected:
     print('Proxy is unavailable')
 except urllib3.exceptions.ProtocolError:
     print('Protocol Error occurred')
-
-
-#free = requests.get('https://ya.ru/', proxies=proxies)
-#print(free)
