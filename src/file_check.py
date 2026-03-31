dbd0f18251cdc8f9f4f065d448725e3a4cfe65cd
Viacheslav Kurganov <slavanik2506@gmail.com>
2026-03-31 19:50:51 +0300

    Changed file_check.py

--- src/file_check.py
+++ src/file_check.py
@@ -1,17 +1,18 @@
-import src.checker.check_single_proxy
+from src.checker.check_single_proxy import check_single_proxy
 
 def to_check_a_file(path_to_file):
     with open(path_to_file, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
     result = list()
+    invalid = {'Invalid proxy', 'Timeout occurred', 'Connection Error occurred'}
 
     for line in lines:
-        if check_single_proxy(line.strip()) == ('Invalid proxy' or 'Tomeout occurred' or 'COnnection Error uccurred':
+        if check_single_proxy(line.strip()) in invalid:
             continue
         else:
-            result.append(check_single_proxy(line.strip())
+            result.append(check_single_proxy(line.strip()))
 
     with open(path_to_file, 'w', encoding='utf-8') as file:
         for res in result:
-            file.writelines(res)
+            file.write(res)
