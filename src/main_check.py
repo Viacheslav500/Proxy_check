27b9adaa4d1b9c0ff04d6edcde656da5b5219683
Viacheslav Kurganov <slavanik2506@gmail.com>
2026-04-03 18:34:56 +0300

    Corrected main file

--- src/main_check.py
+++ src/main_check.py
@@ -5,7 +5,7 @@ from src.checker.file_check import to_check_a_file
 
 def main():
     parser = argparse.ArgumentParser(description='Simple proxy checker. You can check one proxy or a file with list of proxies')
-    parser.add_argument('--proxy', help='Write down only one proxy (http://ip:port)')
+    parser.add_argument('--proxy', '-p', help='Write down only one proxy (http://ip:port)')
     parser.add_argument('--file', '-f', help='Write down a path to the file with list of proxies')
     args = parser.parse_args()
 
@@ -15,7 +15,7 @@ def main():
         to_check_a_file(args.file)
         print('File was checked')
     else:
-        print('Use --proxy or --file to start the script')
+        print('Use --proxy/-p or --file/-f to start the script')
 
 if __name__ == '__main__':
     main()
