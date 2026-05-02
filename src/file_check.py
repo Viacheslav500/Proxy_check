1d900e8a064197af236d2dc66ae46d31e1c78875
Viacheslav Kurganov <slavanik2506@gmail.com>
2026-05-02 14:27:46 +0300

    Correct mistake

--- src/file_check.py
+++ src/file_check.py
@@ -15,7 +15,7 @@ def to_check_a_file(path_to_file):
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
         future_to_proxy = [executor.submit(check_single_proxy, proxy) for proxy in lines]
-        for future in as_completed(future_to_proxy):
+        for future in concurrent.futures.as_completed(future_to_proxy):
             valid = future.result()
             if valid not in invalid:
                 result.append(valid)
